bd3d1c12d222bb0c42d0cccfb52b24309e0222d4
mikepierre <mikeypierre84@yahoo.com>
2015-10-16 13:12:29 -0400

    Create Helper.java
    helper class.

---
+++ installer_j/Helper.java
@@ -0,0 +1,16 @@
+package installer_j;
+
+public class Helper {
+	
+	private String dir_name;
+
+	public void setDir_name(String dir_name) {
+		this.dir_name = dir_name;
+	}
+	
+	public String getDir_name() {
+		return dir_name;
+	}
+
+
+}
